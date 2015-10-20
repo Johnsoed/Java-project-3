86fae7370eb33bb6ccc9171ed3e54b210ea478b7
Lanndon <Lanndon@148.61.121.3>
2015-10-20 11:21:55 -0400

    update the bank Gui class

--- src/BankApplication/BankGui.java
+++ src/BankApplication/BankGui.java
@@ -1,5 +1,27 @@
 package BankApplication;
 
-public class BankGui {
-//gui for the bank project
+import javax.swing.*;
+import javax.swing.JMenuBar;
+
+public class BankGui extends JFrame {
+
+	/**
+	 * gui for the application
+	 */
+	private static final long serialVersionUID = 1L;
+	private JMenuBar menuBar;
+	private JMenu file;
+	private JMenu Sort;
+	private JMenuItem sort_Account;
+	private JMenuItem sort_Owner;
+	private JMenuItem Sort_Date;
+	private JMenuItem file_load_Bin;
+	private JMenuItem file_save_bin;
+	private JMenuItem file_load_Text;
+	private JMenuItem file_save_Text;
+	private JMenuItem file_load_XML;
+	private JMenuItem file_save_XML;
+	private JMenuItem file_Quit;
+	
 }
+ 
